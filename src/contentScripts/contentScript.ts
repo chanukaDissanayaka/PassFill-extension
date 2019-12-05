02fa564f31224aac7a4ffa1153683342f51cf336
Chanuka Dissanayaka <ChanukaD@99x.lk>
2019-12-05 15:54:11 +0530

    updated content script with autofill

--- src/contentScripts/contentScript.ts
+++ src/contentScripts/contentScript.ts
@@ -1,48 +1,84 @@
+function inspectForm(form: HTMLFormElement) {
+    const inputFields = form.getElementsByTagName('input');
+
+    let textCount = 0;
+    let passwordCount = 0;
+
+    // tslint:disable-next-line: prefer-for-of
+    for (let index = 0; index < inputFields.length; index++) {
+        const element = inputFields[index];
+        if (element.type === 'text') {
+            textCount++;
+        } else if (element.type === 'email') {
+            textCount++;
+        } else if (element.type === 'password') {
+            passwordCount++;
+        }
+    }
+
+    const msg = 'textCount : ' + textCount + ', passwordCount : ' + passwordCount;
+    if (textCount === 1 && passwordCount === 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function isALoginPage(): any {
     const forms = document.getElementsByTagName('form');
     if (forms) {
         let loginPageFound = false;
-        let loginForm;
+
         // tslint:disable-next-line: prefer-for-of
         for (let index = 0; index < forms.length; index++) {
             if (!loginPageFound) {
                 const form = forms[index];
-                const inputFields = form.getElementsByTagName('input');
+                loginPageFound = inspectForm(form);
+            }
+        }
+        return loginPageFound;
 
-                let textCount = 0;
-                let passwordCount = 0;
+    } else {
+        return false;
+    }
+}
 
-                // tslint:disable-next-line: prefer-for-of
-                for (let index = 0; index < inputFields.length; index++) {
-                    const element = inputFields[index];
-                    if (element.type === 'text') {
-                        textCount++;
-                    } else if (element.type === 'email') {
-                        textCount++;
-                    } else if (element.type === 'password') {
-                        passwordCount++;
+
+
+function fillForm(username: string, password: string) {
+    const forms = document.getElementsByTagName('form');
+    if (forms) {
+        let loginPageFound = false;
+        // tslint:disable-next-line: prefer-for-of
+        for (let index = 0; index < forms.length; index++) {
+            if (!loginPageFound) {
+                const form = forms[index];
+                loginPageFound = inspectForm(form);
+                alert('found');
+
+                if (loginPageFound) {
+                    const fields = form.getElementsByTagName('input');
+
+                    // tslint:disable-next-line: prefer-for-of
+                    for (let item = 0; item < fields.length; item++) {
+                        const element = fields[item];
+                        if (element.type === 'text' || element.type === 'email') {
+                            console.log('email');
+                            element.value = username;
+                        } else if (element.type === 'password') {
+                            console.log('pass');
+                            element.value = password;
+                        }
                     }
-                }
-
-                const msg = 'textCount : ' + textCount + ', passwordCount : ' + passwordCount;
-                if (textCount === 1 && passwordCount === 1) {
-                    loginPageFound = true;
-                    loginForm = form;
-                    alert('login page found');
                 }
             }
         }
-
-        return loginForm;
     }
-    return null;
 }
 
-// const loginFrom = isALoginPage();
-// if (loginFrom) {
-//     const inputs = loginFrom.getElementsByTagName('input');
-//     for (let index = 0; index < inputs.length; index++) {
-//         const input = inputs[index];
-//         input.text = 'aaaaa';
-//     }
-// }
+const containsLogin = isALoginPage();
+alert(containsLogin);
+if (containsLogin) {
+    fillForm('username', 'password');
+}
+
